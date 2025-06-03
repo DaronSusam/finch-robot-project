b2dfedbd4932145e578bc9f05bd4a5a541a3c8ea
DaronSusam <dsusam0001@mymail.lausd.net>
2025-06-03 12:09:29 -0700

    Update test.java

--- src/main/java/com/ad/test.java
+++ src/main/java/com/ad/test.java
@@ -3,6 +3,10 @@ import com.birdbrain.Finch;
 public class test {
     public static void main(String[] args){
         Finch bird = new Finch();
-        bird.setMove("F", 25, 50);
+        bird.setMove("F", 46, 50);
+        bird.setTurn("L", 90, 50);
+        bird.setMove("F", 20, 50);
+
+        bird.stop();
     }
 }
