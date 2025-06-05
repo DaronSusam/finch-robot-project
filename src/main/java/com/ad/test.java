6a51b0c66020ec39b39f3acb5918694c03c12d79
DaronSusam <dsusam0001@mymail.lausd.net>
2025-06-05 08:32:32 -0700

    Update test.java

--- src/main/java/com/ad/test.java
+++ src/main/java/com/ad/test.java
@@ -3,10 +3,24 @@ import com.birdbrain.Finch;
 public class test {
     public static void main(String[] args){
         Finch bird = new Finch();
-        bird.setMove("F", 46, 50);
+        bird.setMove("F", 105, 50);
         bird.setTurn("L", 90, 50);
-        bird.setMove("F", 20, 50);
+        bird.setMove("F", 40, 50);
+        bird.setTurn("L", 90, 50);
+        bird.setMove("F", 35, 50);
+        bird.setTurn("R", 90, 50);
+        bird.setMove("F", 48, 50);
+        bird.setTurn("R", 90, 50);
+        bird.setMove("F", 115, 50);
 
+
+
+        // int[] distances = {105, 50};
+        // String[] turns = {"R"};
+
+        // for (int distance : distances) {
+        //     bird.setMove("F", distance, 50);
+        // }
         bird.stop();
     }
 }
