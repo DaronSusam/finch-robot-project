914cab26d84fe798dc50fd37ee2ca8cd4ecc3c40
DaronSusam <dsusam0001@mymail.lausd.net>
2025-06-05 09:53:16 -0700

    Finished the code to move. Made more direct measurments.

--- src/main/java/com/ad/test.java
+++ src/main/java/com/ad/test.java
@@ -3,6 +3,12 @@ import com.birdbrain.Finch;
 public class test {
     public static void main(String[] args){
         Finch bird = new Finch();
+
+        /*
+         * This part of our code will move the robot through the maze.
+         *  Measurments done in cm. 
+         * Make sure to put robot a little to the left as it often drifts.
+         */
         bird.setMove("F", 105, 50);
         bird.setTurn("L", 90, 50);
         bird.setMove("F", 40, 50);
@@ -12,6 +18,20 @@ public class test {
         bird.setMove("F", 48, 50);
         bird.setTurn("R", 90, 50);
         bird.setMove("F", 115, 50);
+        bird.setTurn("R", 90, 50);
+        bird.setMove("F", 12,50);
+        bird.setTurn("L", 90, 50);
+        bird.setMove("F", 24, 50);
+        bird.setTurn("L", 90, 50);
+        bird.setMove("F", 20,50);
+
+/*
+ * Plays sound after maze is completed.
+ */
+        // public int getSound()
+
+        // System.out.println("Sound: " + bird.getSound());
+
 
 
 
