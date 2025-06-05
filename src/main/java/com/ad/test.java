12ebca83b9274901f9f7602f9eb06fade691012c
DaronSusam <dsusam0001@mymail.lausd.net>
2025-06-05 10:48:08 -0700

    Added for loops and arrays as well as cleaned up code and comments.

--- src/main/java/com/ad/test.java
+++ src/main/java/com/ad/test.java
@@ -8,39 +8,18 @@ public class test {
          * This part of our code will move the robot through the maze.
          *  Measurments done in cm. 
          * Make sure to put robot a little to the left as it often drifts.
+         * Added for loop and arrays to simplify the code.
          */
-        bird.setMove("F", 105, 50);
-        bird.setTurn("L", 90, 50);
-        bird.setMove("F", 40, 50);
-        bird.setTurn("L", 90, 50);
-        bird.setMove("F", 35, 50);
-        bird.setTurn("R", 90, 50);
-        bird.setMove("F", 48, 50);
-        bird.setTurn("R", 90, 50);
-        bird.setMove("F", 115, 50);
-        bird.setTurn("R", 90, 50);
-        bird.setMove("F", 12,50);
-        bird.setTurn("L", 90, 50);
-        bird.setMove("F", 24, 50);
-        bird.setTurn("L", 90, 50);
-        bird.setMove("F", 20,50);
-
-/*
- * Plays sound after maze is completed.
- */
-        // public int getSound()
-
-        // System.out.println("Sound: " + bird.getSound());
+        int[] distances = {105, 40, 35, 48, 115, 12, 24, 20};
+        String[] turns = {"L", "L", "R", "R", "R", "L", "L"};
+        
+        for (int i = 0; i < distance.length; i++) {
+            bird.setMove("F", distances[i], 50);
+            bird.setTurn(turns[i], 90, 50);
+        }
 
 
 
-
-        // int[] distances = {105, 50};
-        // String[] turns = {"R"};
-
-        // for (int distance : distances) {
-        //     bird.setMove("F", distance, 50);
-        // }
         bird.stop();
     }
 }
